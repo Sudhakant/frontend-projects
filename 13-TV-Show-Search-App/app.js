606674703c29896f07a99d934c5c1d3c22f722a4
sudhakant <sudhakantpanda5@gmail.com>
2024-09-13 21:03:48 +0530

    Get and Append images to the body using TVMaze API via Axios

--- 13-TV-Show-Search-App/app.js
+++ 13-TV-Show-Search-App/app.js
@@ -1,7 +1,12 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function(e){
     e.preventDefault();
     // console.log('Submitted')
     // console.log(form.elements.query.value)
-    const searchTerm = form.elements.qurey.value;
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    // console.log(res.data[0].show.image.medium);
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img);
 })
