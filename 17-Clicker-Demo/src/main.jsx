7498bf0166686d60f4c02636166576b38dedf8f5
sudhakant <sudhakantpanda5@gmail.com>
2024-09-30 15:30:08 +0530

    Clicker Demo

--- 17-Clicker-Demo/src/main.jsx
+++ 17-Clicker-Demo/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Clicker from './Clicker.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Clicker message="Hello" buttonText="Click Me" />
   </StrictMode>,
 )
