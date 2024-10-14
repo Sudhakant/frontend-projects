3dbb305b9e9b257f6329cc0c647ed6dce42f4c63
sudhakant <sudhakantpanda5@gmail.com>
2024-10-14 22:08:50 +0530

    Setting the event value from the input so that the form re-render and the user get this illusion of typing

--- 23-UserForm/src/UserNameForm.jsx
+++ 23-UserForm/src/UserNameForm.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 export default function UserNameForm() {
   const [userName, setUserName] = useState("");
+  const updateUserName = (evt) => {
+    setUserName(evt.target.value);
+  }
   return (
     <div>
-      <input type="text" placeholder="userName" value={userName} />
+      <input type="text" placeholder="userName" value={userName} onChange={updateUserName} />
       <button></button>
     </div>
   );
