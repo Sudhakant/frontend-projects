66dfa1137b483b229ec102fab9badba6a07de18c
sam <50806891+sudhakant@users.noreply.github.com>
2024-10-15 19:38:42 +0530

    React/signup form (#25)
    * Project Initial Setup
    
    * Creating Signup Form with First and Last Name
    
    * Creating an Object for Form
    
    * Making the name of the input SAME as Property to make the form work with Object State
    
    * Changing var and function names
    
    * Handling the submit button for formData
    
    * Computed Property

---
+++ 24-SignupForm/src/BetterSignupForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function BetterSignupForm() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
+  const handleChange = (evt) => {
+    const changedField = evt.target.name;
+    const newValue = evt.target.value;
+
+    setFormData(currData => {
+        return { ...currData, [changedField]: newValue };
+    });
+  }
+
+  const handleSubmit = () => {
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <label htmlFor="firstname">First Name: </label>
+      <input
+        type="text"
+        placeholder="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        name="firstName"
+        id="firstname"
+      />
+      <label htmlFor="lastname">Last Name: </label>
+      <input
+        type="text"
+        placeholder="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        name="firstName"
+        id="lastname"
+      />
+      <button onSubmit={handleSubmit}>Submit</button>
+    </div>
+  );
+}
