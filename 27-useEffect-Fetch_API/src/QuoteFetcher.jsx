9c31d592bb191acb585240f25e11c4e404055546
sudhakant <sudhakantpanda5@gmail.com>
2024-10-17 15:34:46 +0530

    Adding State to Quote

--- 27-useEffect-Fetch_API/src/QuoteFetcher.jsx
+++ 27-useEffect-Fetch_API/src/QuoteFetcher.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 /*
 {
@@ -9,16 +11,21 @@ const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 */
 
 export default function QuoteFetcher(){
+    const [quote, setQuote] = useState({});
+
     async function fetchQuote() {
         const response = await fetch(RANDOM_QUOTE_URL);
         const jsonResponse = await response.json();
         const randomQuote = jsonResponse.quote;
-        console.log(randomQuote);
+        // console.log(randomQuote);
+        setQuote(randomQuote);
     }
 
     return (
         <div>
             <button onClick={fetchQuote}>Get Quote Using Handler</button>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
         </div>
     )
 }
