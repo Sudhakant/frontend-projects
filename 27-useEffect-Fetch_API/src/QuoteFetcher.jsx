adb0a84f6c1a787f4b18f3850274c239cca56899
sam <50806891+sudhakant@users.noreply.github.com>
2024-10-18 10:16:02 +0530

    React/use effect fetch api inspirational quotes (#28)
    * Initial Project Setup
    
    * Getting Quote from API
    
    * Adding State to Quote
    
    * Using useEffect to run the function at the beginning of the component rendering

---
+++ 27-useEffect-Fetch_API/src/QuoteFetcher.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
+/*
+{
+  "quote": {
+    "text": "If you can dream it, you can do it.",
+    "author": "Walt Disney"
+  }
+}
+*/
+
+export default function QuoteFetcher(){
+    const [quote, setQuote] = useState({quote: "", author: ""});
+
+    useEffect(() => {
+      async function fetchQuote() {
+          const response = await fetch(RANDOM_QUOTE_URL);
+          const jsonResponse = await response.json();
+          const randomQuote = jsonResponse.quote;
+          // console.log(randomQuote);
+          setQuote(randomQuote);
+      }
+      fetchQuote();
+    }, []);
+
+    async function fetchQuote() {
+      const response = await fetch(RANDOM_QUOTE_URL);
+      const jsonResponse = await response.json();
+      const randomQuote = jsonResponse.quote;
+      // console.log(randomQuote);
+      setQuote(randomQuote);
+  }
+
+    return (
+        <div>
+            <button onClick={fetchQuote}>Get Quote Using Handler</button>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
+        </div>
+    )
+}
